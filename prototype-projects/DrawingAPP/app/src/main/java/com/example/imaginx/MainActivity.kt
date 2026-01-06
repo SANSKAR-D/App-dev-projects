8fea3182d979e2ea50873b86006781f08c1b4aa0
SANSKAR-D <sethsanskar856@gmail.com>
2026-01-06 23:46:26 +0530

    Drawing App start

---
+++ prototype-projects/DrawingAPP/app/src/main/java/com/example/imaginx/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.example.imaginx
+
+import android.app.Dialog
+import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.SeekBar
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var drawingView: DrawingView
+    private lateinit var brushButton: ImageButton
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        drawingView = findViewById(R.id.drawing_view)
+        brushButton = findViewById(R.id.brush_button)
+        drawingView.changeBrushSize(23.toFloat())
+
+        brushButton.setOnClickListener {
+            showBrushChooserDialog()
+        }
+    }
+    private fun showBrushChooserDialog(){
+        val brushDialog = Dialog(this@MainActivity)
+        brushDialog.setContentView(R.layout.dialog_brush)
+        val seekBarProgress = brushDialog.findViewById<SeekBar>(R.id.dialog_seekbar)
+        val showProgressTv = brushDialog.findViewById<TextView>(R.id.seekbar_progress_text)
+        seekBarProgress.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(
+                seekBar: SeekBar?,
+                progress: Int,
+                fromUser: Boolean
+            ) {
+                drawingView.changeBrushSize(seekBar?.progress!!.toFloat())
+                showProgressTv.text = seekBar.progress.toString()
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+        })
+        brushDialog.show()
+    }
+}
